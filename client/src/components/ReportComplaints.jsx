4304018b2f2384b227f4d9a2c3dd76762aedbe5b
Shubham Verma <vshubham799@gmail.com>
2022-06-08 14:13:39 -0700

    feat: Common popup Code implemented #12

--- client/src/components/ReportComplaints.jsx
+++ client/src/components/ReportComplaints.jsx
@@ -1,67 +1,97 @@
-import React from "react";
-
+import React, { useState } from "react";
+import PopUp from "./Footer/ModelPopups/PopUp";
+import "./ReportComplaint.css";
 let ReportComplaints = () => {
-    
+  let [popUp, setPopUp] = useState(false);
+  let TogglePopUp = () => {
+    setPopUp(!popUp);
+  };
+
+  let PopUpContent;
+  if (popUp) {
+    PopUpContent = <PopUp TogglePopUp={TogglePopUp} />;
+  }
+
   return (
     <>
       <div className="sitePage">
         <h1>Report Complaint</h1>
 
         <form action="/report-complaint" method="POST">
-          <label>
-            Title of Complaint
-            <input type="text" name="title" />
-          </label>
+          <div className="labelInputWrapper">
+            <label>
+              Title of Complaint
+              <input type="text" name="title" />
+            </label>
+          </div>
+          <div className="labelInputWrapper">
+            <label>
+              Description of Complaint
+              <input type="text" name="description" />
+            </label>
+          </div>
+          <div className="labelInputWrapper">
+            <label for="img">
+              Upload image:
+              <input type="file" id="img" name="img" accept="image/*"></input>
+            </label>
+          </div>
+          <div className="labelInputWrapper">
+            <label>
+              Name
+              <input type="text" name="name" />
+            </label>
+          </div>
 
-          <label>
-            Description of Complaint
-            <input type="text" name="description" />
-          </label>
+          <div className="labelInputWrapper">
+            <label for="isLocked">
+              Is Locked:
+              <select name="isLocked" id="isLocked">
+                <option value="true">True</option>
+                <option value="false">False</option>
+              </select>
+            </label>
+          </div>
+          <div className="labelInputWrapper">
+            <label>
+              Report Date:
+              <input type="date" name="reportDate" />
+            </label>
+          </div>
+          <div className="LocationWrapper">
+            <label>
+              Location
+              <a className="LocationBtn" onClick={TogglePopUp}>
+                Get Location
+              </a>
+            </label>
+          </div>
 
-          <label for="img">
-            Upload image:
-            <input type="file" id="img" name="img" accept="image/*"></input>
-          </label>
-
-          <label>
-            Name
-            <input type="text" name="name" />
-          </label>
-
-          <label for="isLocked">
-            Is Locked:
-            <select name="isLocked" id="isLocked">
-              <option value="true">True</option>
-              <option value="false">False</option>
-            </select>
-          </label>
-
-          <label>
-            Report Date:
-            <input type="date" name="reportDate" />
-          </label>
-
-          <label>
-            Contact No.:
-            <input
-              type="number"
-              placeholder="between 0 and 9"
-              value="0"
-              name="contact"
-            />
-          </label>
-
-          <label>
-            Priority:
-            <input
-              type="number"
-              min="1"
-              max="5"
-              placeholder="between 1 and 5"
-              value="1"
-              name="priority"
-            />
-          </label>
+          {PopUpContent}
+          <div className="labelInputWrapper">
+            <label>
+              Contact No.:
+              <input
+                type="number"
+                placeholder="between 0 and 9"
+                value="0"
+                name="contact"
+              />
+            </label>
+          </div>
+          <div className="labelInputWrapper">
+            <label>
+              Priority:
+              <input
+                type="number"
+                min="1"
+                max="5"
+                placeholder="between 1 and 5"
+                value="1"
+                name="priority"
+              />
+            </label>
+          </div>
 
           <input type="submit" value="Registered Complaint" />
         </form>
