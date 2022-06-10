baa841484521fe88ca7596cb4e3cffc604415d17
Parminder <paramindersingh0025@gmail.com>
2022-06-09 22:43:19 -0700

    code refactor

--- server/index.js
+++ server/index.js
@@ -42,7 +42,9 @@ mongoose
   });
 
 // SV: Here are all my routes which you will also mention when any endpoint for the api is created.
+//****** User Routes ****** 
 app.use("/user", UserRoutes);
+//****** Complaint Routes ****** 
 app.use("/complaint", ComplaintRoutes);
 
 // let say you wanted to fetch the data but we need to first perform authentication.
--- server/models/Report.js
+++ server/models/Report.js
@@ -1,53 +1,52 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const ReportSchema = new Schema({
-    title: {
-        type: String,
-        minlength: 1,
-        maxlength:20,
-        required: true
-    },
-    description: {
-            type: String,
-            required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
+  title: {
+    type: String,
+    minlength: 1,
+    maxlength: 20,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
 
-    img:{
-        type:File
-    },
+  img: {
+    type: File,
+  },
 
-    reportDate: {
-        type:Date
-    },
+  reportDate: {
+    type: Date,
+  },
 
-    isLocked:{
-        type: Boolean,
-        required: true
-    },
-    location:{
-        type:Object
-    },
-    
-    priority: {
-        type: Number,
-        min: 0,
-        max: 5
-    },
-    userId:{
-        type:Object
-    },
-    ngoId:{
-        type:Object
-    },
-    contact: {
-        type: String
-    }
-})
+  isLocked: {
+    type: Boolean
+  },
+  location: {
+    type: Object,
+  },
 
-exports.Report = mongoose.model('Report', ReportSchema)
+  priority: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  userId: {
+    type: Object,
+  },
+  ngoId: {
+    type: Object,
+  },
+  contact: {
+    type: String,
+  },
+});
+
+exports.Report = mongoose.model("Report", ReportSchema);
