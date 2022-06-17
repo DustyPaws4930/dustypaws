6f7774731730df8dc16b09377fa176fa9fe8cf4a
123-Teju <59676568+123-Teju@users.noreply.github.com>
2022-06-17 09:05:40 -0700

    event page slider on card has been implemented

--- client/src/components/Event/Event.jsx
+++ client/src/components/Event/Event.jsx
@@ -2,6 +2,7 @@
 import { Link } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel'
 import './Event.css';
+import Header from '../Header/Header';
 
 const Event = props => {
 
@@ -18,6 +19,7 @@ const Event = props => {
 
   return (
     <>
+    <Header />
     <div className='featured-event'>
       <img src="" alt="featured Event" />
       <h2>fetch heading from thr create event </h2>
@@ -25,127 +27,156 @@ const Event = props => {
       <Link to='/singleEvent'><button>View More</button></Link>
     </div>
     
-      <Carousel>
-          <div className='card'>
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
-              </div>
+    <div className='slider'>
+            <div className='slides'>
+              <input type='radio' name='radio-btn' id='radio1' />
+              <input type='radio' name='radio-btn' id='radio2' />
+              <input type='radio' name='radio-btn' id='radio3' />
+              <input type='radio' name='radio-btn' id='radio4' />
 
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
-              </div>
-          
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
-              </div>
-          
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
-              </div>
-          </div>
-      </Carousel>
+            <div className='slide'>
 
 
-      <div>
-          <h1>Upcoming Week</h1>
-       <Carousel>
-          <div className='card'>
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
+              <div className='card'>
+                    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div> 
+    
+            </div>
+
+
+            </div>
+
+              <div className='navigation-auto'>
+                  <div className='auto-btn1'></div>
+                  <div className='auto-btn2'></div>
+                  <div className='auto-btn3'></div>
+                  <div className='auto-btn4'></div>
               </div>
 
+              <div className='navigation-manual'>
+                  <label for='radio1' className='manual-btn'></label>
+                  <label for='radio2' className='manual-btn'></label>
+                  <label for='radio3' className='manual-btn'></label>
+                  <label for='radio4' className='manual-btn'></label>
+              </div>
+
+
+            </div>
       
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
-              </div>
-      
-        
-              <div className='card-body'>
-                  <div className='card-date'>
-                    <p>display date </p>
-                  </div>
-                  <img src="" alt="" />
-                  <h2 className='card-title'>title</h2>
-                  <img src="" alt="wishlist" />
-                  <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                  <Link to='/singleEvent'><p>View More...</p></Link>
-              </div>
-          </div>
-       </Carousel>
-
+        </div>
 
         <h1>Our partners</h1>
-        <Carousel>
-            <div className='card'>
-              <div className='card-body'>
-                    <img src="" alt="" />
-                    <h2 className='card-title'>NGO name</h2>
-                    <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                </div>
+        <div className='slider'>
+            <div className='slides'>
+              <input type='radio' name='radio-btn' id='radio5' />
+              <input type='radio' name='radio-btn' id='radio6' />
+              <input type='radio' name='radio-btn' id='radio7' />
+              <input type='radio' name='radio-btn' id='radio8' />
 
-                <div className='card-body'>
-                    <img src="" alt="" />
-                    <h2 className='card-title'>NGO name</h2>
-                    <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                </div>
+            <div className='slide'>
 
-                <div className='card-body'>
-                    <img src="" alt="" />
-                    <h2 className='card-title'>NGO name</h2>
-                    <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                </div>
 
-                <div className='card-body'>
-                    <img src="" alt="" />
-                    <h2 className='card-title'>NGO name</h2>
-                    <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                </div>
-              
+              <div className='card'>
+                    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div> 
+    
             </div>
-        </Carousel>
-      </div>
+
+
+            </div>
+
+              <div className='navigation-auto'>
+                  <div className='auto-btn5'></div>
+                  <div className='auto-btn6'></div>
+                  <div className='auto-btn7'></div>
+                  <div className='auto-btn8'></div>
+              </div>
+
+              <div className='navigation-manual'>
+                  <label for='radio5' className='manual-btn'></label>
+                  <label for='radio6' className='manual-btn'></label>
+                  <label for='radio7' className='manual-btn'></label>
+                  <label for='radio8' className='manual-btn'></label>
+              </div>
+
+
+            </div>
+      
+        </div>
 
     </>
   )
--- client/src/components/Event/EventForm.jsx
+++ client/src/components/Event/EventForm.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom'
 import './Event.css';
-// import { trash } from '../images/trash-can'
+import Header from "../Header/Header";
+
 
 const EventForm = () => {
 
-
-  const breakPoints = [
-    {width: 1, itemsToShow: 1},
-    {width: 500, itemsToShow: 2},
-    {width: 1000, itemsToShow: 3},
-  ]
-
   return (
     <>
+      <Header/>
       <div className='event-ngo'>
           <form action="">
                 <div className="event-form">
@@ -67,62 +61,81 @@ const EventForm = () => {
 
       <div>
         <h3>Created Events</h3>
-        
-            <Carousel breakPoints = {breakPoints}>
-            <div className='card'>
-                  <div className='card-body'>
-                      <div className='card-date'>
-                        <p>display date </p>
-                      </div>
-                      <img src="" alt="" />
-                      <h2 className='card-title'>title</h2>
-                      <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                      <Link to='/singleEvent'><button>View More...</button></Link>
-                      {/* <i className='fa fa-heart-o'></i> */}
-                      <img src='' alt="dustbin" />
-                      <Link to='/eventEdit'><img src="" alt="edit" /></Link>
-                  </div>
-            
-                  <div className='card-body'>
-                      <div className='card-date'>
-                        <p>display date </p>
-                      </div>
-                      <img src="" alt="" />
-                      <h2 className='card-title'>title</h2>
-                      <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                      <Link to='/singleEvent'><button>View More...</button></Link>
-                      <img src="" alt="dustbin" />
-                      <Link to='/eventEdit'><img src="" alt="edit" /></Link>
-                  </div>
-              
-                  <div className='card-body'>
-                      <div className='card-date'>
-                        <p>display date </p>
-                      </div>
-                      <img src="" alt="" />
-                      <h2 className='card-title'>title</h2>
-                      <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                      <Link to='/singleEvent'><button>View More...</button></Link>
+        <div className='slider'>
+            <div className='slides'>
+              <input type='radio' name='radio-btn' id='radio1' />
+              <input type='radio' name='radio-btn' id='radio2' />
+              <input type='radio' name='radio-btn' id='radio3' />
+              <input type='radio' name='radio-btn' id='radio4' />
 
-                      <img src="" alt="dustbin" />
-                      <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+            <div className='slide'>
+
+
+              <div className='card'>
+                    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    <div className='card-body'>
+                        <div className='card-date'>
+                          <p>display date </p>
+                        </div>
+                        <img src="" alt="" />
+                        <h2 className='card-title'>title</h2>
+                        <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+                        <Link to='/singleEvent'><button>View More...</button></Link>
+                        <i className='fa fa-heart-o'></i> 
+                        <img src='' alt="dustbin" />
+                        <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+                    </div>
+    
+                    
+    
                   </div>
 
-                  <div className='card-body'>
-                      <div className='card-date'>
-                        <p>display date </p>
-                      </div>
-                      <img src="" alt="" />
-                      <h2 className='card-title'>title</h2>
-                      <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
-                      <Link to='/singleEvent'><button>View More...</button></Link>
 
-                      <img src="" alt="dustbin" />
-                      <Link to='/eventEdit'><img src="" alt="edit" /></Link>
-                  </div>
-                  </div>
-            </Carousel>
-       
+            </div>
+
+              <div className='navigation-auto'>
+                  <div className='auto-btn1'></div>
+                  <div className='auto-btn2'></div>
+                  <div className='auto-btn3'></div>
+                  <div className='auto-btn4'></div>
+              </div>
+
+              <div className='navigation-manual'>
+                  <label for='radio1' className='manual-btn'></label>
+                  <label for='radio2' className='manual-btn'></label>
+                  <label for='radio3' className='manual-btn'></label>
+                  <label for='radio4' className='manual-btn'></label>
+              </div>
+
+
+            </div>
+        </div>
       </div>
 
 
@@ -135,3 +148,18 @@ const EventForm = () => {
 }
 
 export default EventForm
+
+
+//  <div className='card-body'>
+// <div className='card-date'>
+//   <p>display date </p>
+// </div>
+// <img src="" alt="" />
+// <h2 className='card-title'>title</h2>
+// <p className='card-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum harum possimus ut quia architecto ipsa. Unde deleniti consectetur veniam soluta? Minima dolorem vitae incidunt totam maxime fugit similique soluta quod.</p>
+// <Link to='/singleEvent'><button>View More...</button></Link>
+//  <i className='fa fa-heart-o'></i> */}
+// <img src='' alt="dustbin" />
+// <Link to='/eventEdit'><img src="" alt="edit" /></Link>
+// </div> 
+
