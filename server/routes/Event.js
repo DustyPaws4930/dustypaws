abfcf1addd04127e4e75c437c581a2cf1a5c95ab
Shubham Verma <vshubham799@gmail.com>
2022-06-19 23:06:20 -0700

    Added validation in the event field

--- server/routes/Event.js
+++ server/routes/Event.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { Register } from "../controllers/event/Register";
+import { CheckFormFields } from "../middlewares/EventValidation.js";
 import ImageUpload from "../middlewares/ImageUpload.js";
 
 const router = express.Router();
 
-router.post("/register", ImageUpload, Register);
+router.post("/register", CheckFormFields, ImageUpload, Register);
 
 export default router;
