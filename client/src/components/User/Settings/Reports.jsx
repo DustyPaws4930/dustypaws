90c8067b9ea797e2fbe2f0d6771a3dd2289dadb8
Shubham Verma <vshubham799@gmail.com>
2022-06-22 18:00:05 -0700

    Added the content of the rest of the pages in profile page

--- client/src/components/User/Settings/Reports.jsx
+++ client/src/components/User/Settings/Reports.jsx
@@ -4,6 +4,22 @@ const Reports = () => {
   return (
     <div className="userReportContainer">
       <h1>Reported Cases</h1>
+      <div className="CasesContainer">
+        <div className="caseWrapper">
+          <p>Case Title</p>
+          <button>Icon Image</button>
+        </div>
+        <div className="caseWrapper">
+          <p>Case Title</p>
+          <button>Icon Image</button>
+        </div>
+        <div className="caseWrapper">
+          <p>Case Title</p>
+          <button>Icon Image</button>
+        </div>
+
+        <button>View All</button>
+      </div>
     </div>
   );
 };
--- client/src/components/User/Settings/Whislist.jsx
+++ client/src/components/User/Settings/Whislist.jsx
@@ -4,6 +4,22 @@ const Whislist = () => {
   return (
     <div className="wishlistEvents">
       <h1>Whishlisted Events</h1>
+      <div className="eventContainer">
+        <div className="eventWrapper">
+          <p>Evetn Title</p>
+          <button>Icon Image</button>
+        </div>
+        <div className="eventWrapper">
+          <p>Event Title</p>
+          <button>Icon Image</button>
+        </div>
+        <div className="eventWrapper">
+          <p>Evetn Title</p>
+          <button>Icon Image</button>
+        </div>
+
+        <button>View All</button>
+      </div>
     </div>
   );
 };
