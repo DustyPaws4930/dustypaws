852f6facfc80b473a154dfb2710c38b844672994
123-Teju <59676568+123-Teju@users.noreply.github.com>
2022-07-07 21:52:20 -0700

    Complete css of about us page - responsive

--- client/src/components/Donate/Donate.jsx
+++ client/src/components/Donate/Donate.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import './Donate.css'
 
 const Donate = (props) => {
+
+  const [show, setShow] = useState(true)  
+ 
+  const Style = {
+    display: "grid"
+
+  }
   return (
     <>
       <header className="header">
@@ -64,29 +71,56 @@ const Donate = (props) => {
                 {/* <div><button>Donate</button></div> */}
             </div>
           </div>
-
-          <div className="donate-card-img">
-            <div className="donate-card-dark">
-                <div className="donate-card-content">
-                  <h2>People For Animal</h2>
-                  <p>We are dedicated to help stray animals across India we are providing our services across 20 states.</p>
-                  <div className="card-btn"><button>Donate</button></div>
+</section>
+          {/* <div className="card-load-more"> */}
+          {show ? null : 
+              <section className="donate-card1" style={Style}>
+                  <div className="donate-card-img1">
+                  <div className="donate-card-dark">
+                      <div className="donate-card-content">
+                        <h2>People For Animal</h2>
+                        <p>We are dedicated to help stray animals across India we are providing our services across 20 states.</p>
+                        <div className="card-btn"><button>Donate</button></div>
+                      </div>
+                  </div>
                 </div>
-                {/* <div><button>Donate</button></div> */}
-            </div>
-          </div>
-
-          <div className="donate-card-img">
-            <div className="donate-card-dark">
-                <div className="donate-card-content">
-                  <h2>People For Animal</h2>
-                  <p>We are dedicated to help stray animals across India we are providing our services across 20 states.</p>
-                  <div className="card-btn"><button>Donate</button></div>
+    
+                <div className="donate-card-img1">
+                  <div className="donate-card-dark">
+                      <div className="donate-card-content">
+                        <h2>People For Animal</h2>
+                        <p>We are dedicated to help stray animals across India we are providing our services across 20 states.</p>
+                        <div className="card-btn"><button>Donate</button></div>
+                      </div>
+                  </div>
                 </div>
-                {/* <div><button>Donate</button></div> */}
-            </div>
-          </div>
+
+                <div className="donate-card-img1">
+                  <div className="donate-card-dark">
+                      <div className="donate-card-content">
+                        <h2>People For Animal</h2>
+                        <p>We are dedicated to help stray animals across India we are providing our services across 20 states.</p>
+                        <div className="card-btn"><button>Donate</button></div>
+                      </div>
+                      
+                  </div>
+                </div>
+
+                <div className="donate-card-img1">
+                  <div className="donate-card-dark">
+                      <div className="donate-card-content">
+                        <h2>People For Animal</h2>
+                        <p>We are dedicated to help stray animals across India we are providing our services across 20 states.</p>
+                        <div className="card-btn"><button>Donate</button></div>
+                      </div>
+                  </div>
+                </div>
         </section>
+}
+      {show ?
+        <div className="card-load-more" onClick={()=>setShow(false)}><button >Load More</button></div>
+        :
+        null}
       </main>
 
 
