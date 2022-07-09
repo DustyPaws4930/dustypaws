dd82380c28d53803dcb0aedf316c68128c21d3a7
Shubham Verma <vshubham799@gmail.com>
2022-07-08 20:50:09 -0700

    User Infor css done

--- client/src/components/User/Settings/Info.jsx
+++ client/src/components/User/Settings/Info.jsx
@@ -78,7 +78,7 @@ const Info = () => {
         }}
       >
         <div className="headerContainer">
-          <h3>Test Username</h3>
+          <h2>Test Username</h2>
         </div>
         <div className="userImage">
           <img src="" alt="" />
@@ -88,105 +88,109 @@ const Info = () => {
             <h3>About</h3>
 
             <div className="containerInfoWrapper">
-              <h5>Contact Information</h5>
-              <div className="labelInputWrapper">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  name="username"
-                  value={userInfo.username || ""}
-                  onChange={(e) => {
-                    HandleOnChange(e);
-                  }}
-                  id="username"
-                />
-              </div>
-
-              <div className="labelInputWrapper">
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <input
-                  type="text"
-                  name="phoneNumber"
-                  value={userInfo.phoneNumber}
-                  onChange={(e) => {
-                    HandleOnChange(e);
-                  }}
-                  id="phoneNumber"
-                />
-              </div>
-
-              <div className="labelInputWrapper">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  value={userInfo.email}
-                  onChange={(e) => {
-                    HandleOnChange(e);
-                  }}
-                  id="email"
-                />
-              </div>
-
-              <div className="AddressInfoContainer">
+              <h4 className="ContactInfoHeader">Contact Information</h4>
+              <div className="upperInfoWrapper">
                 <div className="labelInputWrapper">
-                  <label htmlFor="address">Address</label>
+                  <label htmlFor="username">Username</label>
                   <input
                     type="text"
-                    value={userInfo.address}
+                    name="username"
+                    value={userInfo.username || ""}
                     onChange={(e) => {
                       HandleOnChange(e);
                     }}
-                    name="address"
-                    id="address"
+                    id="username"
                   />
+                </div>
+
+                <div className="labelInputWrapper">
+                  <label htmlFor="phoneNumber">Phone Number</label>
+                  <input
+                    type="text"
+                    name="phoneNumber"
+                    value={userInfo.phoneNumber}
+                    onChange={(e) => {
+                      HandleOnChange(e);
+                    }}
+                    id="phoneNumber"
+                  />
+                </div>
+
+                <div className="labelInputWrapper">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={userInfo.email}
+                    onChange={(e) => {
+                      HandleOnChange(e);
+                    }}
+                    id="email"
+                  />
+                </div>
+
+                <div className="AddressInfoContainer">
+                  <div className="labelInputWrapper">
+                    <label htmlFor="address">Address</label>
+                    <input
+                      type="text"
+                      value={userInfo.address}
+                      onChange={(e) => {
+                        HandleOnChange(e);
+                      }}
+                      name="address"
+                      id="address"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
             <div className="basicInfoWrapper">
-              <h5>Basic Information</h5>
+              <h4 className="ContactInfoHeader">Basic Information</h4>
+              <div className="midInfoWrapper">
+                <div className="labelInputWrapper">
+                  <label htmlFor="dob">Date of Birth</label>
+                  <input
+                    type="date"
+                    name="Dob"
+                    value={userInfo.Dob}
+                    onChange={(e) => {
+                      HandleOnChange(e);
+                    }}
+                    id="Dob"
+                  />
+                </div>
 
-              <div className="labelInputWrapper">
-                <label htmlFor="dob">Date of Birth</label>
-                <input
-                  type="date"
-                  name="Dob"
-                  value={userInfo.Dob}
-                  onChange={(e) => {
-                    HandleOnChange(e);
-                  }}
-                  id="Dob"
-                />
-              </div>
-
-              <div className="labelInputWrapper">
-                <label htmlFor="gender">Gender</label>
-                <select
-                  name="gender"
-                  value={userInfo.gender}
-                  onChange={(e) => {
-                    HandleOnChange(e);
-                  }}
-                  id="name"
-                >
-                  {allGenders.map((gender, idx) => {
-                    return (
-                      <option key={idx} value={gender}>
-                        {gender}
-                      </option>
-                    );
-                  })}
-                </select>
+                <div className="labelInputWrapper">
+                  <label htmlFor="gender">Gender</label>
+                  <select
+                    name="gender"
+                    value={userInfo.gender}
+                    onChange={(e) => {
+                      HandleOnChange(e);
+                    }}
+                    id="name"
+                  >
+                    {allGenders.map((gender, idx) => {
+                      return (
+                        <option key={idx} value={gender}>
+                          {gender}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
               </div>
             </div>
             <div className="securityWrapper">
-              <h5>Security and Privacy</h5>
+              <h3>Security and Privacy</h3>
 
               <div className="labelInputWrapper">
                 <label htmlFor="password">Update Password</label>
                 <input
                   type="password"
                   name="password"
+                  placeholder="New password"
                   value={userInfo.password}
                   onChange={(e) => {
                     HandleOnChange(e);
